c7fe5a8c774151e6b647661b44db5cb1a46dac07
Ilya <ilyaafon5@gmail.com>
2025-06-25 20:55:34 +0300

    Fix add button color

--- client/src/components/primitives/add-button.tsx
+++ client/src/components/primitives/add-button.tsx
@@ -1,5 +1,6 @@
 import { Icon } from '../icon/icon';
 import { Button } from './styled/button';
+import {theme} from "../../theme/theme";
 
 type Props = {
   onClick: () => void;
@@ -7,7 +8,7 @@ type Props = {
 
 const AddButton = ({ onClick }: Props) => {
   return (
-    <Button className="add-btn" onClick={onClick} color='transparent'>
+    <Button className="add-btn" onClick={onClick} color={theme.colors.N30}>
       <Icon iconName="add" />
     </Button>
   );
